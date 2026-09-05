1579bf2708c493d3caaeff1f8edb96c966f58ad8
Hyria Roman <hyrya.roman@gmail.com>
2026-09-05 12:48:41 +0300

    models: add user model

---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+
+export const usersSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+usersSchema.index({ email: 1 });
+
+export const Users = model('Users', usersSchema);
