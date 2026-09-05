95df1ff18f408851433aa2e940c01f88031ca637
Hyria Roman <hyrya.roman@gmail.com>
2026-09-05 13:19:41 +0300

    services: add auth service

---
+++ src/services/auth.js
@@ -0,0 +1,38 @@
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/time.js';
+import { Sessions } from '../models/session.js';
+
+export function createSession(userId) {
+  const accessToken = crypto.randomUUID();
+  const refreshToken = crypto.randomUUID();
+
+  return Sessions.create({
+    userId,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
+  });
+}
+
+export function setSessionCookies(response, session) {
+  response.cookie('accessToken', session.accessToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    maxAge: FIFTEEN_MINUTES,
+  });
+
+  response.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    maxAge: ONE_DAY,
+  });
+
+  response.cookie('sessionId', session._id, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    maxAge: ONE_DAY,
+  });
+}
