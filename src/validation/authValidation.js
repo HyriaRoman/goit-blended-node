2b1044bc070a048cc91aed0cdd17fc769a9caab2
Hyria Roman <hyrya.roman@gmail.com>
2026-09-05 13:20:06 +0300

    validation: add validation for /auth/register

---
+++ src/validation/authValidation.js
@@ -0,0 +1,26 @@
+import { Joi, Segments } from 'celebrate';
+
+export const registerSchema = {
+  [Segments.BODY]: Joi.object({
+    name: Joi.string().optional().messages({
+      'string.base': '`name` must be a string',
+      'string.min': '`name` should have at least {#limit} characters',
+    }),
+
+    email: Joi.string().email().required().messages({
+      'any.required': '`email` is required',
+      'string.base': '`email` must be a string',
+      'string.min': '`email` should have at least {#limit} characters',
+    }),
+
+    password: Joi.string().required().min(8).messages({
+      'any.required': '`password` is required',
+      'string.base': '`password` must be a string',
+      'string.min': '`password` should have at least {#limit} characters',
+    }),
+  })
+    .required()
+    .messages({
+      'any.required': 'must have a body',
+    }),
+};
