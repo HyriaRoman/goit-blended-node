704d4bb3b4cf86e0b46c64fd3b7b2e5f2254f27c
Hyria Roman <hyrya.roman@gmail.com>
2026-09-19 12:17:09 +0300

    misc: formatting

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 import { loginSchema, registerSchema } from '../validation/authValidation.js';
-import { login, logout, refreshToken, register } from '../controllers/authController.js';
+import {
+  login,
+  logout,
+  refreshToken,
+  register,
+} from '../controllers/authController.js';
 
 const router = Router();
 
@@ -9,6 +14,5 @@ router.post('/auth/register', celebrate(registerSchema), register);
 router.post('/auth/login', celebrate(loginSchema), login);
 router.post('/auth/logout', logout);
 router.post('/auth/refresh', refreshToken);
-
 
 export default router;
