cfb0fdb8bb1a094f0f2471a8305bfc6f676d5500
Hyria Roman <hyrya.roman@gmail.com>
2026-09-19 12:16:38 +0300

    routes: add '/auth/logout'

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 import { loginSchema, registerSchema } from '../validation/authValidation.js';
-import { login, refreshToken, register } from '../controllers/authController.js';
+import { login, logout, refreshToken, register } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerSchema), register);
 router.post('/auth/login', celebrate(loginSchema), login);
+router.post('/auth/logout', logout);
 router.post('/auth/refresh', refreshToken);
 
 
