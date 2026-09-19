bb5778cec0bec631157dbb1d7a56ac99eb3c58fe
Hyria Roman <hyrya.roman@gmail.com>
2026-09-19 11:01:28 +0300

    validation: add validation for /auth/login

--- src/validation/authValidation.js
+++ src/validation/authValidation.js
@@ -3,9 +3,9 @@ import { Joi, Segments } from 'celebrate';
 export const registerSchema = {
   [Segments.BODY]: Joi.object({
     name: Joi.string().optional().messages({
-      'any.required': '`name` is required',
-      'string.base': '`name` must be a string',
-      'string.min': '`name` should have at least {#limit} characters',
+      'any.required': '`email` is required',
+      'string.base': '`email` must be a string',
+      'string.email': '`email` must be a valid email',
     }),
 
     email: Joi.string().email().required().messages({
@@ -18,6 +18,21 @@ export const registerSchema = {
       'any.required': '`password` is required',
       'string.base': '`password` must be a string',
       'string.min': '`password` should have at least {#limit} characters',
+    }),
+  }),
+};
+
+export const loginSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required().messages({
+      'any.required': '`email` is required',
+      'string.base': '`email` must be a string',
+      'string.email': '`email` must be a valid email',
+    }),
+
+    password: Joi.string().required().messages({
+      'any.required': '`password` is required',
+      'string.base': '`password` must be a string',
     }),
   }),
 };
